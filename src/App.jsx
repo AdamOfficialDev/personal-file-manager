48b8ad8aa17adfb0d21c2c28ed747a15d1deb58e
AdamOfficialDev <adamabdillah808@gmail.com>
2024-09-09 07:54:39 +0700

    Memperbarui root path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FileManager from "./components/FileManager";
-// import Home from "./components/Home";
+// import Home from "./components/Home"; // Jika masih ada Home atau komponen lain
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-base-200 p-6">
-        <Routes>
-          <Route path="/" element={<FileManager />} />
-          {/* <Route path="/folder/*" element={<FileManager />} /> */}
-        </Routes>
-      </div>
+      <Routes>
+        {/* Rute utama yang mengarah ke FileManager */}
+        <Route path="/" element={<FileManager />} />
+        {/* Rute lain jika ada */}
+        <Route path="/folder/*" element={<FileManager />} />
+        {/* Misalnya rute lain yang mungkin ada */}
+        {/* <Route path="/home" element={<Home />} /> */}
+      </Routes>
     </Router>
   );
 }
